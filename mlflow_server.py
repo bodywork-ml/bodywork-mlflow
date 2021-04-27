b3950edb465d2d27f4b81762b0ae5394ec4f0357
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-04-27 14:18:23 +0100

    Run Black on Python file.

--- mlflow_server.py
+++ mlflow_server.py
@@ -11,7 +11,7 @@ from mlflow.server.handlers import initialize_backend_stores
 from mlflow.utils.process import ShellCommandException
 
 
-DEFAULT_HOST = '0.0.0.0'
+DEFAULT_HOST = "0.0.0.0"
 DEFAULT_PORT = 5000
 
 
@@ -19,10 +19,7 @@ def configure_logger() -> logging.Logger:
     """Configure a logger that will write to stdout."""
     log_handler = logging.StreamHandler(sys.stdout)
     log_format = logging.Formatter(
-        '%(asctime)s - '
-        '%(levelname)s - '
-        '%(module)s.%(funcName)s - '
-        '%(message)s'
+        "%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s"
     )
     log_handler.setFormatter(log_format)
     log = logging.getLogger(__name__)
@@ -30,10 +27,8 @@ def configure_logger() -> logging.Logger:
     log.setLevel(logging.INFO)
     return log
 
-def start_mlflow_server(
-    backend_store_uri: str,
-    default_artifact_root: str
-) -> None:
+
+def start_mlflow_server(backend_store_uri: str, default_artifact_root: str) -> None:
     """Start the server.
 
     :param backend_store_uri: URI to a database back-end.
@@ -42,7 +37,7 @@ def start_mlflow_server(
     try:
         initialize_backend_stores(backend_store_uri, default_artifact_root)
     except Exception as e:
-        log.error(f'Error initializing backend store - {e}')
+        log.error(f"Error initializing backend store - {e}")
         sys.exit(1)
 
     try:
@@ -51,39 +46,39 @@ def start_mlflow_server(
             default_artifact_root,
             DEFAULT_HOST,
             DEFAULT_PORT,
-            workers=1
+            workers=1,
         )
     except ShellCommandException as e:
-        log.error(f'Running the mlflow server failed - {e}')
+        log.error(f"Running the mlflow server failed - {e}")
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     log = configure_logger()
 
     try:
         import sentry_sdk
-        sentry_dsn = os.environ.get('SENTRY_DSN')
+
+        sentry_dsn = os.environ.get("SENTRY_DSN")
         sentry_sdk.init(sentry_dsn, traces_sample_rate=1.0)
     except ModuleNotFoundError:
-        log.warning('Sentry SDK not available - monitoring not enabled')
+        log.warning("Sentry SDK not available - monitoring disabled")
     except KeyError:
-        log.warning('environment variable SENTRY_DSN not found - monitoring not enabled')
+        log.warning("environment variable SENTRY_DSN not found - monitoring disabled")
 
     try:
-        backend_store_uri = os.environ['MLFLOW_BACKEND_STORE_URI']
+        backend_store_uri = os.environ["MLFLOW_BACKEND_STORE_URI"]
     except KeyError:
-        log.error('environment variable MLFLOW_BACKEND_STORE_URI cannot be found')
+        log.error("environment variable MLFLOW_BACKEND_STORE_URI cannot be found")
         sys.exit(1)
 
     try:
-        default_artifact_root = os.environ['MLFLOW_DEFAULT_ARTIFACT_ROOT']
+        default_artifact_root = os.environ["MLFLOW_DEFAULT_ARTIFACT_ROOT"]
     except KeyError:
-        log.error('environment variable MLFLOW_DEFAULT_ARTIFACT_ROUTE cannot be found')
+        log.error("environment variable MLFLOW_DEFAULT_ARTIFACT_ROUTE cannot be found")
         sys.exit(1)
 
-    log.info('starting MLflow server')
+    log.info("starting MLflow server")
     start_mlflow_server(
-        backend_store_uri=backend_store_uri,
-        default_artifact_root=default_artifact_root
+        backend_store_uri=backend_store_uri, default_artifact_root=default_artifact_root
     )
